90c0771bb9df7749f8f3ca9704448d85cbcf67db
Guilherme Teixeira <75620226+GuilhermeTeixeira13@users.noreply.github.com>
2022-10-22 18:04:52 +0100

    Side menu at Screen 1 and Toolbar with share button working well.

--- SpyfallUBI/app/src/main/java/pt/ubi/di/pmd/spyfall_ubi/MainActivity.java
+++ SpyfallUBI/app/src/main/java/pt/ubi/di/pmd/spyfall_ubi/MainActivity.java
@@ -4,10 +4,12 @@ import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.drawerlayout.widget.DrawerLayout;
-
 import android.os.Bundle;
 import android.widget.ImageView;
-import android.widget.TextView;
+import android.content.Intent;
+import android.view.Menu;
+import android.view.MenuItem;
+import androidx.annotation.NonNull;
 
 public class MainActivity extends AppCompatActivity {
     private DrawerLayout drawer;
@@ -17,19 +19,52 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ImageView menuIcon = findViewById(R.id.menu_icon);
-        ImageView shareIcon = findViewById(R.id.share_icon);
-        TextView title = findViewById(R.id.landing_tootlbar_title);
         ImageView spyImage = findViewById(R.id.spyImage);
 
-        Toolbar sideMenu = findViewById(R.id.side_menu);
+        Toolbar sideMenu = findViewById(R.id.toolbar_landing_page);
         setSupportActionBar(sideMenu);
 
         drawer = findViewById(R.id.drawer_layout);
 
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, sideMenu,
-                R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, sideMenu, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.addDrawerListener(toggle);
         toggle.syncState();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+
+        // first parameter is the file for icon and second one is menu
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        // We are using switch case because multiple icons can be kept
+        switch (item.getItemId()) {
+            case R.id.shareButton:
+
+                Intent sharingIntent = new Intent(Intent.ACTION_SEND);
+
+                // type of the content to be shared
+                sharingIntent.setType("text/plain");
+
+                // Body of the content
+                String shareBody = "Link to Playstore";
+
+                // subject of the content. you can share anything
+                String shareSubject = "Spyfall @ UBI!";
+
+                // passing body of the content
+                sharingIntent.putExtra(Intent.EXTRA_TEXT, shareBody);
+
+                // passing subject of the content
+                sharingIntent.putExtra(Intent.EXTRA_SUBJECT, shareSubject);
+                startActivity(Intent.createChooser(sharingIntent, "Share using"));
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
